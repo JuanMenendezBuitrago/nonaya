ec9c567d5dac6537c53e760e96267fc598afa14a
Juan Menendez <juan.menendez.buitrago@gmail.com>
2024-10-13 18:19:04 -0500

    Constantes para textos

---
+++ constants/texts.ts
@@ -0,0 +1,77 @@
+export const FERMENTATION_TYPES = {
+    FERMENTATION_CARBONIC: {
+        NAME: 'FERMENTATION_CARBONIC',
+        ES:   'Carbónica'
+    },
+    FERMENTATION_ANAEROBIC: {
+        NAME: 'FERMENTATION_ANAEROBIC',
+        ES:   'Anaeróbica'
+    },
+    FERMENTATION_YEAST: {
+        NAME: 'FERMENTATION_YEAST',
+        ES:   'Por levaduras'
+    }
+}
+
+export const DIALOGS_HEADERS = {
+    COFFEE: {
+        CREATE: {ES: 'Nuevo café'},        
+        EDIT:   {ES: 'Editar café'},
+        DELETE: {ES: 'Eliminar café'},
+    },
+    METHOD: {
+        CREATE: {ES: 'Nuevo método'},        
+        EDIT:   {ES: 'Editar método'},
+        DELETE: {ES: 'Eliminar método'},
+    },
+    VARIETY: {
+        CREATE: {ES: 'Nueva variedad'},        
+        EDIT:   {ES: 'Editar variedad'},
+        DELETE: {ES: 'Eliminar variedad'},
+    },
+    PROCESS: {
+        CREATE: {ES: 'Nuevo proceso'},        
+        EDIT:   {ES: 'Editar proceso'},
+        DELETE: {ES: 'Eliminar proceso'},
+    },
+    NOTE: {
+        CREATE: {ES: 'Nueva nota'},        
+        EDIT:   {ES: 'Editar nota'},
+        DELETE: {ES: 'Eliminar nota'},
+    },
+    ORIGIN: {
+        CREATE: {ES: 'Nuevo origen'},        
+        EDIT:   {ES: 'Editar origen'},
+        DELETE: {ES: 'Eliminar origen'},
+    },
+    CITY: {
+        CREATE: {ES: 'Nueva ciudad'},        
+        EDIT:   {ES: 'Editar ciudad'},
+        DELETE: {ES: 'Eliminar ciudad'},
+    },
+    COUNTRY: {
+        CREATE: {ES: 'Nuevo país'},        
+        EDIT:   {ES: 'Editar país'},
+        DELETE: {ES: 'Eliminar país'},
+    },
+    REGION: {
+        CREATE: {ES: 'Nueva región'},        
+        EDIT:   {ES: 'Editar región'},
+        DELETE: {ES: 'Eliminar región'},
+    },
+    COUPON: {
+        CREATE: {ES: 'Nuevo cupón'},        
+        EDIT:   {ES: 'Editar cupón'},
+        DELETE: {ES: 'Eliminar cupón'},
+    },
+    ROAST: {
+        CREATE: {ES: 'Nuevo tueste'},        
+        EDIT:   {ES: 'Editar tueste'},
+        DELETE: {ES: 'Eliminar tueste'},
+    },
+    GRIND: {
+        CREATE: {ES: 'Nueva molienda'},        
+        EDIT:   {ES: 'Editar molienda'},
+        DELETE: {ES: 'Eliminar molienda'},
+    },        
+}
